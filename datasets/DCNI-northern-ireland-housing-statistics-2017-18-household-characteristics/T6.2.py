8002045d8feaf4fd36e86dc3f6bcad84aa2591ce
VTula2000 <36479963+VTula2000@users.noreply.github.com>
2020-02-11 16:29:00 +0000

    Basic Tidy data

---
+++ datasets/DCNI-northern-ireland-housing-statistics-2017-18-household-characteristics/T6.2.py
@@ -0,0 +1,43 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from gssutils import *
+
+# +
+cell = tab.filter(contains_string('Household Size'))
+cell.assert_one()
+Year = cell.shift(1,0).fill(RIGHT).is_not_blank().is_not_whitespace() 
+size = cell.fill(DOWN).is_not_blank().is_not_whitespace()
+observations = Year.shift(0,5).fill(DOWN).is_not_blank().is_not_whitespace()
+Dimensions = [
+            HDim(Year,'Year',DIRECTLY,ABOVE),
+            HDim(size,'NI Household Characteristics',DIRECTLY,LEFT ),
+            HDimConst('Unit','people'),  
+            HDimConst('Measure Type','Count'),
+            HDimConst('NI Household Description','Northern Ireland Household by type')
+    
+]  
+c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
+new_table = c1.topandas()
+import numpy as np
+new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'NI Marker'}, inplace=True)
+new_table['Period'] = new_table['Year'].map(
+    lambda x: {
+        '2012' : 'year/2012', 
+        '2022' : 'year/2022',
+        '2037': 'year/2037' ,
+        'Projected Change 2012-2037': 'gregorian-interval/2012-03-31T00:00:00/P15Y'
+        }.get(x, x))
+new_table['Geography'] = 'Northern Ireland'
---
+++ datasets/DCNI-northern-ireland-housing-statistics-2017-18-household-characteristics/T6.4.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from gssutils import *
+
+# +
+cell = tab.filter(contains_string('Tenure Type'))
+cell.assert_one()
+size = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
+ttype = cell.fill(DOWN).is_not_blank().is_not_whitespace()
+observations = size.fill(DOWN).is_not_blank().is_not_whitespace()
+Dimensions = [
+            HDim(ttype, 'ttype',DIRECTLY,LEFT)
+            HDim(size,'NI Household Characteristics',DIRECTLY,ABOVE),
+            HDimConst('Unit','number'),  
+            HDimConst('Measure Type','Count'),
+            HDimConst('NI Household Description','Northern Ireland Accommodation By Tenure')
+    
+]  
+c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
+new_table = c1.topandas()
+import numpy as np
+new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'NI Marker'}, inplace=True)
+new_table['Period'] = 'gregorian-interval/2017-03-31T00:00:00/P1Y'
+new_table['NI Household Characteristics'] = new_table['NI Household Characteristics'] + '' + new_table['ttype']
+new_table['NI Household Characteristics'] = new_table['NI Household Characteristics'].str.rstrip('2')
+new_table['Geography'] = 'Northern Ireland'
