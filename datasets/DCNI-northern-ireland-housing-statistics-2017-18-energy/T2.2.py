22ed5721dd64cc8cebc8c745a13df2af533ab8fd
VTula2000 <36479963+VTula2000@users.noreply.github.com>
2020-02-13 10:45:52 +0000

    Basic Tidy data need to update with scraper

---
+++ datasets/DCNI-northern-ireland-housing-statistics-2017-18-energy/T2.2.py
@@ -0,0 +1,36 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from gssutils import *
+
+# +
+cell = tab.excel_ref('A3')
+Year = cell.fill(RIGHT).is_not_blank().is_not_whitespace() 
+sap = cell.fill(DOWN).is_not_blank().is_not_whitespace()
+observations = Year.shift(0,1).fill(DOWN).is_not_blank().is_not_whitespace() -tab.excel_ref('B21')
+Dimensions = [
+            HDim(Year,'Year',DIRECTLY,ABOVE),
+            HDim(sap,'NI Household Energy',DIRECTLY,LEFT ),
+            HDimConst('Unit','sap scale'),  
+            HDimConst('Measure Type','Rate'),
+            HDimConst('NI Household Description','Northern Ireland Household energy SAP Rating')
+    
+]  
+c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
+new_table = c1.topandas()
+import numpy as np
+new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'NI Marker'}, inplace=True)
+new_table['Period'] = 'year/' + new_table['Year']
+new_table['Geography'] = 'Northern Ireland'
